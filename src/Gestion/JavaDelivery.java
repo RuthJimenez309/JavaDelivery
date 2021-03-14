3ee1b398ea0bff448685971eeeb15740dac1a060
RuthJimenez309 <79241546+RuthJimenez309@users.noreply.github.com>
2021-03-14 00:06:35 -0600

    create java class

---
+++ src/Gestion/JavaDelivery.java
@@ -0,0 +1,11 @@
+package Gestion;
+
+public class JavaDelivery {
+
+    public String getMarkets (String categoria){
+        //debera retornar todos los negocios que cumplan esta categoria.
+        //return ;
+    }
+
+
+}
